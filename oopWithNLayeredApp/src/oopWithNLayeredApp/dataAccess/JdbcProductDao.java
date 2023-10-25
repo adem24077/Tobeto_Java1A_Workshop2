78e8d3a4ed695f03420e0521812341cdf1527869
Adem Furkan Kavuş <adem.24077@gmail.com>
2023-10-25 16:01:41 +0300

    Tobeto Workshop 2 25.10.2023

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,13 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao{
+	public void add(Product product) {
+		//Sadece ve sadece db erişim kodları buraya yazılır... SQL bilgisi gerekir.
+		System.out.println("JDBC ile veri tabanına eklendi.");
+	}
+	
+}
+
+//Hibernate
